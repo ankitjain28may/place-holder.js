c7b7604ce41413d61f4f3c25ff2b3435876709d1
ankitjain28may <ankitjain28may77@gmail.com>
2016-11-09 12:55:33 +0530

    Modified place-holder.js with less if-else ladder

--- place-holder.js
+++ place-holder.js
@@ -1,71 +1,19 @@
-function isText(type) {
-  if (type === "text") {
-    return true;
-  }
-  return false;
-}
+var prefix = "Enter the";
 
-function isEmail(type) {
-  if (type === "email") {
-    return true;
-  }
-  return false;
-
-}
-
-function isNumber(type) {
-  if (type === "number") {
-    return true;
-  }
-  return false;
-
-}
-
-function isPassword(type) {
-  if (type === "password") {
-    return true;
-  }
-  return false;
-
-}
-
-function isTel(type) {
-  if (type === "tel") {
-    return true;
-  }
-  return false;
-
-}
-
-function isURL(type) {
-  if (type === "url") {
-    return true;
-  }
-  return false;
-
-}
+var type = {
+  'text' : ' Text',
+  'tel' : ' Mobile Number',
+  'password' : ' Password',
+  'url' : ' URL',
+  'number' : ' Number',
+  'email' : ' Email Address'
+};
 
 var ele = $(":input");
 var len = ele.length;
 var i = 0;
-var type = "";
-for (i = 0; i < len; i++) {
-  type = ele[i].type;
-  if (!ele[i].placeholder) {
-    if (isText(type)) {
-      ele[i].placeholder = "Enter the Text";
-    } else if (isEmail(type)) {
-      ele[i].placeholder = "Enter the Email Address";
-    } else if (isNumber(type)) {
-      ele[i].placeholder = "Enter the Number";
-    } else if (isPassword(type)) {
-      ele[i].placeholder = "Enter the Password";
-    } else if (isTel(type)) {
-      ele[i].placeholder = "Enter the Mobile No.";
-    } else if (isURL(type)) {
-      ele[i].placeholder = "Enter the URL";
-    }
-  }
-}
+for (i = 0; i < len; i+=1)
+  if (!ele[i].placeholder)
+    ele[i].placeholder = prefix + type[ele[i].type];
 
 $("textarea").attr("placeholder", "Enter the message");
